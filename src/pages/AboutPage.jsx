cc56f80425b17991ec8e811b0ac3a3f44940261b
matheesha27 <matheesha27@gmail.com>
2026-06-05 17:11:08 +0530

    Changes About-us

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -2,7 +2,7 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { motion } from "framer-motion";
 
-export default function LoginPage() {
+export default function AboutPage() {
 
   return (
     <div className="min-h-screen bg-secondary text-white flex flex-col">
